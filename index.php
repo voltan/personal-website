8cde55a6df3c7e3aa053f2f30cde76601cdd425b
voltan <djvoltan@gmail.com>
2017-02-04 13:52:54 +0330

    Work on website

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
     <link href="./css/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css">
     <link href="./css/locale.css" media="screen" rel="stylesheet" type="text/css">
     <link href="./css/style.css" media="screen" rel="stylesheet" type="text/css">
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-91384236-1', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
 <body id="pi-system" class="pi-locale-fa">
 <div class="container clearfix">
